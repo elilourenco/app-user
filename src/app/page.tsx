ff5d74b94db1a23c4b20c815d4096bf8e97f6ff9
unknown <lourencomonica318@gmail.com>
2023-04-04 21:01:24 +0100

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 
-import Image from "next/image"
+
 import * as React from 'react';
 import Header from "./componentes/Header/page"
-import svg from "../../public/assets/mobiledevices.svg"
+
+
+import { Input } from '@chakra-ui/react'
+
+
 
 
 
@@ -16,28 +20,9 @@ export default function Home() {
       
       
       <main> 
+     
 
-      
-  
-    
-
-
-        <p>MAKE YOUR BUY NEW</p>
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy
-          text ever since the 1500s.
-         
-        </p>
-         <Image 
-         
-         alt="image svg"
-         src={svg}
-         width={607}
-         height={304}
-         />
-        <p>50€</p>
-        <p>per month</p>
-        <button>SUBSCRIBE</button>
+    <Input placeholder='Basic usage' />
       </main>
      
 
@@ -46,3 +31,6 @@ export default function Home() {
       
   )
 }
+
+
+
